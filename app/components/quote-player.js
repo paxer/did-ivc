eeba5bb357c68a6dee9b7f55643b472122274adf
Pavel Kotlyar <pavel.kotlyar@icloud.com>
2018-04-20 18:15:35 +1000

    add image on play

--- app/components/quote-player.js
+++ app/components/quote-player.js
@@ -9,12 +9,14 @@ export default Component.extend({
     play(quote) {
       $('ul#quotes_list li').removeClass('active');
       this.$('li').addClass('active');
+      $('img#ded_image').fadeIn();
 
       this.get('audio').src = `/assets/sounds/${quote}.mp3`;
       this.get('audio').play();
 
       this.get('audio').onended = () => {
         this.$('li').removeClass('active');
+        $('img#ded_image').fadeOut();
       };
     }
   }
